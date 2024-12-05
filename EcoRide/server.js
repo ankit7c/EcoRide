53ed7602bd8416d8fe26aaf0c6f7dcc150b5f9f6
Rohan <rspatil3@illinois.edu>
2024-12-04 19:30:17 -0600

    Added CarRating and EcoPoints Route

--- EcoRide/server.js
+++ EcoRide/server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
 var bcrypt = require('bcrypt');
 const path = require('path');
 const { kMaxLength } = require('buffer');
-
-
-dotenv.config();
-
-
+const moment = require('moment');
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -18,23 +12,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+const connection = require('./db');
+const { checkUserRole, getUserIdByUsername, getBookingsByUserId, getUserDetailsByName, getUserCarsByUserName, updateEcoPointsForUser, addToCarRating} = require('./utils/help');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-  
-connection.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err.stack);
-    return;
-  }
-  console.log("Connected to GCP SQL database.");
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
   res.render('index'); 
@@ -114,39 +97,49 @@ app.post('/login', function (req, res) {
 
 app.get('/:username/checkRole', (req, res) => {
   const username = req.params.username;
-
-  const getUserIdQuery = 'SELECT userId FROM User WHERE name = ?';
-
-  connection.query(getUserIdQuery, [username], (err, results) => {
-    if (err) {
-      console.error('Error fetching user ID:', err);
-      return res.status(500).send('Error fetching user ID');
-    }
-
-    if (results.length === 0) {
-      return res.status(404).send('User not found');
-    }
-
-    const userId = results[0].userId; 
-
+  getUserIdByUsername(username, (err,resultUser)=>{
+    const userId = resultUser
     const checkUserRoleQuery = 'SELECT role FROM UserRoles WHERE userId = ?';
-
+    
     connection.query(checkUserRoleQuery, [userId], (err, roleResults) => {
       if (err) {
         console.error('Error checking user role:', err);
         return res.status(500).send('Error checking user role');
       }
-      console.log(roleResults)
-      console.log(roleResults.length)
       if (roleResults.length > 0) {
-        // res.render('')
-        res.redirect(`/${username}/${roleResults[0].role}/profile?param=${roleResults.length}`);
+            res.redirect(`/${username}/${roleResults[0].role}/profile?param=${roleResults.length}`)
       }else{
         res.redirect(`/${username}/manage`)
       }
     });
-  });
+  })
 });
+
+app.get('/:username/:role/profile', (req, res) => {
+  var { param } = req.query; 
+  const { username, role } = req.params; 
+  getUserDetailsByName(username, (err, resultUser)=>{
+    const user = resultUser
+    getUserCarsByUserName(user.userId,(err, carResults)=>{
+      const cars = carResults
+      getBookingsByUserId(user.userId, (err, resultBooking)=>{
+        const bookingResults = resultBooking
+        const rewardQuery = 'SELECT points FROM EcoPoints where userId =?'
+        connection.query(rewardQuery,[user.userId], (err,resReward)=>{
+          const pts = resReward[0].points
+          checkUserRole(user.userId, (err,resultRole)=>{
+            param = resultRole.length
+            const role = param>1? "seller":"buyer"
+            res.render('profile',{username, user, role, param, cars, bookingResults, pts})
+          })
+        })
+      })
+    })
+  })
+});
+
+
+
 
 
 app.get("/:username/manage", (req,res)=>{
@@ -156,57 +149,33 @@ app.get("/:username/manage", (req,res)=>{
 
 // can be updated in future with the param value 
 app.get('/:username/addRole', (req, res) => {
-  console.log("Reached ADD role")
   const { username } = req.params;
   const { role } = req.query;  
-  console.log(role)
-  console.log(typeof(role))
   if (role !== 'buyer' && role !== 'seller') {
     return res.status(400).send('Invalid role');
   }
-  const getUserIdQuery = 'SELECT userId FROM User WHERE name = ?';
-  connection.query(getUserIdQuery, [username], (err, results) => {
-    if (err) {
-      console.error('Error fetching user ID:', err);
-      return res.status(500).send('Error fetching user ID');
-    }
-
-    if (results.length === 0) {
-      return res.status(404).send('User not found');
-    }
-
-    const userId = results[0].userId; 
-    console.log("Inside add role",userId)
-    
+  getUserIdByUsername(username, (err, resultUser)=>{
+    const userId = resultUser 
     const getLatestUserRoleIdQuery = 'SELECT MAX(userRoleId) AS latestUserRoleID FROM UserRoles';
-    // connection.query(getUserIdQuery, [username], (err, results) => {
-    
+    connection.query(getLatestUserRoleIdQuery, (err, result) => {
+      if (err) {
+        console.error('Error fetching latest UserRoleID:', err);
+        return res.status(500).send('Error fetching latest UserRoleID');
+      }
 
-      connection.query(getLatestUserRoleIdQuery, (err, result) => {
+      const latestUserRoleID = result[0].latestUserRoleID || 0; 
+      const newUserRoleID = latestUserRoleID + 1
+      const updateRoleQuery = `INSERT INTO UserRoles (userRoleID, userId, role) VALUES (?, ?, ?);`;
+      connection.query(updateRoleQuery, [newUserRoleID , userId, role], function(err, result) {
         if (err) {
-          console.error('Error fetching latest UserRoleID:', err);
-          return res.status(500).send('Error fetching latest UserRoleID');
+            res.status(500).send({ message: 'Error registering user', error: err });
+            return;
         }
-  
-        const latestUserRoleID = result[0].latestUserRoleID || 0; // Get the latest UserRoleID, default to 0 if empty
-        const newUserRoleID = latestUserRoleID + 1; // Increment the UserRoleID
-  
-        // Query to insert or update the role in the UserRoles table with the new UserRoleID
-        const updateRoleQuery = `
-          INSERT INTO UserRoles (userRoleID, userId, role) 
-          VALUES (?, ?, ?);
-        `;
-        connection.query(updateRoleQuery, [newUserRoleID , userId, role], function(err, result) {
-          if (err) {
-              res.status(500).send({ message: 'Error registering user', error: err });
-              return;
-          }
-          console.log(username)
-          console.log(role)
-          res.redirect(`/${username}/${role}/profile`);
-      });
-  
-      });
+        checkUserRole(userId, (err,resultRole)=>{
+          res.redirect(`/${username}/${role}/profile?param=${resultRole.length}`);
+        })
+      })
+    })  
   });
 });
 
@@ -214,9 +183,12 @@ app.get('/:username/addRole', (req, res) => {
 app.get('/:username/cars/book', (req, res) => {
   const searchTerm = req.query.search || ''; 
   const sql = `
-      SELECT * FROM Car 
+      SELECT c.userId, c.carId,c.mileage,c.price,c.carCompany,c.carModel,ROUND(avg(cr.ratingValue),1) as ratingValue
+      FROM Car c
+      LEFT JOIN CarRating cr ON c.carId=cr.carId
       WHERE availability=true
       AND (carModel LIKE ? OR carCompany LIKE ?)
+      Group by c.carId;
   `;
 
   connection.query(sql, [`%${searchTerm}%`, `%${searchTerm}%`], (err, results) => {
@@ -253,7 +225,6 @@ app.post('/:username/car/:carId/book/confirm', (req, res) => {
   const { username, carId } = req.params;
   const startTime = new Date();
   const endTime = new Date(startTime.getTime() + hours * 60 * 60 * 1000);
-  console.log("Confirm TT",endTime)
 
   const getUserIdQuery = 'SELECT userId FROM User WHERE name = ?';
 
@@ -282,13 +253,13 @@ app.post('/:username/car/:carId/book/confirm', (req, res) => {
       const nextBookingId = (result[0].maxId || 0) + 1;
 
       const insertQuery = `
-        INSERT INTO Booking (bookingId, carId, userId, startDate, endDate, startMileage, endMileage)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO Booking (bookingId, carId, userId, startDate, endDate, startMileage, endMileage, tripStatus)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
       `;
 
       connection.query(
         insertQuery,
-        [nextBookingId, carId, userId, startTime, endTime, startMileage, startMileage, hours],
+        [nextBookingId, carId, userId, startTime, endTime, startMileage, startMileage, 0],
         (err, result) => {
           if (err) {
             return res.status(500).send({ message: 'Error inserting booking', error: err });
@@ -328,7 +299,6 @@ app.get('/:username/seller/add-car', (req, res) => {
   
   
 app.post('/:username/seller/add-car', (req, res) => {
-  console.log("Seller has started adding car")
   const { carModel, mileage, price, availability, carCompany } = req.body; 
   const { username } = req.params; 
   var availabilityBool = availability.toLowerCase() === "true";
@@ -389,74 +359,10 @@ app.post('/:username/seller/add-car', (req, res) => {
     });
   });
 });  
-
-
-app.get('/:username/:role/profile', (req, res) => {
-  const { username, role } = req.params; 
-  const { param } = req.query;  
-  const getUserDetails = 'SELECT userId, name, email FROM User WHERE name = ?';
-  
-  const getUserCars = 'SELECT * FROM Car WHERE userId = (SELECT userId FROM User WHERE name = ?)';
-
-  connection.query(getUserDetails, [username], (err, userResult) => {
-    if (err || userResult.length === 0) {
-      res.status(500).send({ message: 'Error fetching user details or user not found', error: err });
-      return;
-    }
-    const user = userResult[0];
-    console.log(user)
-
-    connection.query(getUserCars, [username], (err, carResults) => {
-      if (err) {
-        res.status(500).send({ message: 'Error fetching user cars', error: err });
-        return;
-      }
-      console.log("HIIII", username)
-      console.log("HIIII", user)
-      console.log("HIIII", carResults)
-      const getBookingsQuery = `
-        SELECT b.startDate, b.endDate,b.endMileage, c.carModel, c.carCompany, c.price
-        FROM Booking b
-        INNER JOIN Car c ON b.carId = c.carId
-        INNER JOIN User u ON b.userId = u.userId
-        WHERE u.userId = ?
-      `;
-
-      connection.query(getBookingsQuery, [user.userId], (bookingErr, bookingResults) => {
-        if (bookingErr) {
-          res.status(500).send({ message: 'Error fetching bookings', error: bookingErr });
-          return;
-        }
-        res.render('profile', { username, user, cars: carResults, role, param, bookingResults});
-      });
-    });
-  });
-});
-
-      // const getBookingsQuery = `
-      //   SELECT b.startDate, b.endDate,b.endMileage, c.carModel, c.carCompany, c.price
-      //   FROM Booking b
-      //   INNER JOIN Car c ON b.carId = c.carId
-      //   INNER JOIN User u ON b.userId = u.userId
-      //   WHERE u.userId = ?
-      // `;
-
-      // connection.query(getBookingsQuery, [user.userId], (bookingErr, bookingResults) => {
-      //   if (bookingErr) {
-      //     res.status(500).send({ message: 'Error fetching bookings', error: bookingErr });
-      //     return;
-      //   }
-      //   console.log("Booking data",bookingResults)
-
-        
-      // });
       
-
-  
 
 app.post('/:username/seller/:carId/delete', (req, res) => {
   const { username, carId } = req.params;
-  console.log("inside post request to delete",carId)
   // Query to delete the car based on carId
   const deleteCarQuery = 'DELETE FROM Car WHERE carId = ?';
 
@@ -496,8 +402,6 @@ app.post('/:username/seller/:carId/delete', (req, res) => {
 });
   
 app.get('/:username/car/:carId/edit', (req, res) => {
-  
-  console.log("i'm inside edit car")
   const { username, carId } = req.params;
 
   const getCarQuery = 'SELECT carModel, carCompany, mileage, price, availability FROM Car WHERE carId = ?';
@@ -578,9 +482,138 @@ app.post('/:username/car/:carId/edit', (req, res) => {
     }
   );
 });
+
+app.get('/:username/car/:carId/:carCompany/:carModel/end-ride', (req, res) => {
+  const { username, carId, carCompany, carModel } = req.params;
+
+  const getUserIdQuery = 'SELECT userId FROM User WHERE name = ?';
+  
+  connection.query(getUserIdQuery, [username], (err, userResult) => {
+    if (err || userResult.length === 0) {
+      return res.status(500).send({ message: 'Error fetching user details or user not found', error: err });
+    }
+
+    const userId = userResult[0].userId;
+
+    const getBookingDetailsQuery = `
+      SELECT bookingId, startDate, endDate, startMileage, endMileage 
+      FROM Booking 
+      WHERE carId = ? AND userId = ? AND tripStatus = 0
+    `;
+
+    connection.query(getBookingDetailsQuery, [carId, userId], (err, bookingResult) => {
+      if (err) {
+        return res.status(500).send({ message: 'Error fetching booking details', error: err });
+      }
+
+      if (bookingResult.length === 0) {
+        return res.status(404).send({ message: 'No ongoing trip found for this car and user' });
+      }
+      const getCarPrice = 'SELECT price FROM Car WHERE carId = ?'
+      connection.query(getCarPrice, [carId], (err, carPrice)=>{
+        if (err) {
+          return res.status(500).send({ message: 'Error fetching car price', error: err })
+        }
+        const booking = bookingResult[0];
+        const price = carPrice[0].price
+        res.render('end-ride', { username, carId, carCompany, carModel, price, booking });
+
+      })
+
+      
+    });
+  });
+});
+
+
+app.post('/:username/car/:carId/end-ride/:bookingId', (req, res) => {
+  const { username, carId, bookingId } = req.params;
+  const endMileage = parseFloat(parseFloat(req.body.endMileage).toFixed(2));
+  const rating = parseFloat(req.body.rating);
+  console.log(rating)
+  try {
+    const getStartMileageQuery = 'SELECT mileage, price from Car WHERE carId = ?';
+    connection.query(getStartMileageQuery,carId,(err,resultMil)=>{
+      const startMileage = parseFloat(parseFloat(resultMil[0].mileage).toFixed(2));
+      const mileageDifference = endMileage - startMileage;
+      const updatedMileage = (endMileage + startMileage) / 2;
+      const updateAvailabilityQuery = 'UPDATE Car SET availability = 1, mileage=? WHERE carId = ?';
+      connection.query(updateAvailabilityQuery, [updatedMileage,carId], (err,results)=>{
+        getUserIdByUsername(username,(err,resultUserId)=>{
+          const userId = resultUserId
+          // buyer ecopoints
+          if(mileageDifference>5)
+          {
+            updateEcoPointsForUser(userId, (err, updateEco)=>{})
+          }
+          // seller ecopoints
+          if(rating>=4)
+          {
+            const carUserQuery = 'SELECT userId FROM Car WHERE carId = ?'
+            connection.query(carUserQuery,[carId],(err,carUser)=>{
+              const carUserId = carUser[0].userId
+              console.log("hi from the car owner",carUser)
+              updateEcoPointsForUser(carUserId, (err, updateSellerEco)=>{})
+            })
+          }
+          getBookingsByUserId(userId,(err,resbook)=>{
+            const getBookingDetailsByBookingIdQuery ='SELECT * FROM Booking WHERE bookingId = ?'
+            connection.query(getBookingDetailsByBookingIdQuery, [bookingId],(err, bookResOne)=>{
+              
+              const startDateTime = new Date(bookResOne[0].startDate);  
+              const endDateTime = new Date(bookResOne[0].endDate);      
+              const returnDateTime = new Date();
+              
+              const basePricePerHour = resultMil[0].price
+              const billingEndTime = returnDateTime <= endDateTime ? endDateTime : returnDateTime; 
+              const billingDurationMs = billingEndTime - startDateTime;
+              const billingDurationHours = Math.ceil(billingDurationMs / (1000 * 60 * 60));
+              const basePrice = billingDurationHours * basePricePerHour;
+
+              let fine = 0;  
+              if (returnDateTime > endDateTime) {
+                const overtimeMs = returnDateTime - endDateTime;
+                const overtimeHours = Math.ceil(overtimeMs / (1000 * 60 * 60));
+                fine = overtimeHours * 5; 
+              }
+              const totalPrice = basePrice + fine;
+              const updateBookingQuery = 'UPDATE Booking SET tripStatus = 1, endDate = NOW(), tripCost=?, endMileage=? WHERE bookingId = ?';
+              connection.query(updateBookingQuery, [totalPrice,endMileage,bookingId],(err,resultsT)=>{
+                checkUserRole(userId, (err,resultRole)=>{
+                  const roleLen = resultRole.length
+                  const role = roleLen>1? "seller":"buyer"
+                  addToCarRating(carId, userId, bookingId, rating,(err, resRating)=>{
+
+                    res.redirect(`/${username}/${role}/profile?param=${roleLen}`);
+                  })  
+                });
+              })
+            })
+          })
+        });
+      })
+    });
+  }catch (err) {
+    console.error('Error ending ride:', err);
+    res.status(500).send({ message: 'Error processing ride completion', error: err });
+  }
+});
   
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
